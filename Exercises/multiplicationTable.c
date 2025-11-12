08946f279cbebd630502613b3ab53b57ff848d57
Sebastian <45011245+Sebastian54@users.noreply.github.com>
2025-11-12 13:47:32 +0000

    finished

--- Exercises/multiplicationTable.c
+++ Exercises/multiplicationTable.c
@@ -7,7 +7,7 @@ int main()
     printf("******************************\n");
     printf("Enter a number:> ");
     if (scanf("%d", &number) != 1) return 1;
-
+    
     printf("\nx%d Multiplication Table\n", number);
     for (int i = 1; i <= 12; ++i) {
         printf("%d x %d = %d\n", i, number, i * number);
