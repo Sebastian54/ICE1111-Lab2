84b39a83d4aef90bd64d15e438ae30fb3b211b18
Sebastian <45011245+Sebastian54@users.noreply.github.com>
2025-11-18 15:24:05 +0000

    fixed

--- Assessed/fpaOperations.c
+++ Assessed/fpaOperations.c
@@ -18,15 +18,15 @@ int main()
     printf("Enter a double  :> "); // Prompt the user for double2
     scanf("%lf", &double2); // Read the input from the user
 
-    // using %.2lf to format the output to 2 decimal places below, 
+    // using %.2lf to format the output to 2 decimal places below,
     // source https://www.w3schools.com/c/c_data_types_dec.php && https://www.w3schools.com/c/c_type_conversion.php
     
-    printf("\nSum: %.2lf", double1 + double2); // Output the sum of double1 and double2
-    printf("\nDifference: %.2lf", double1 - double2); // Output the difference of double1 and double2
-    printf("\nProduct: %.2lf", double1 * double2); // Output the product of double1 and double2
-    printf("\nQuotient: %.2lf", double1 / double2); // Output the quotient of double1 and double2
-    printf("\nMean: %.2lf", (double1 + double2) / 2); // Output the mean of double1 and double2
-    printf("\nMax: %.2lf", (double1 > double2) ? double1 : double2); // Output the maximum of double1 and double2 
-    printf("\nMin: %.2lf", (double1 < double2) ? double1 : double2); // Output the minimum of double1 and double2
+    printf("\nSum:              %5.2lf", double1 + double2); // Output the sum of double1 and double2
+    printf("\nDifference:       %5.2lf", double1 - double2); // Output the difference of double1 and double2
+    printf("\nProduct:          %5.2lf", double1 * double2); // Output the product of double1 and double2
+    printf("\nQuotient:         %5.2lf", double1 / double2); // Output the quotient of double1 and double2
+    printf("\nMean:             %5.2lf", (double1 + double2) / 2); // Output the mean of double1 and double2
+    printf("\nMax:              %5.2lf", (double1 > double2) ? double1 : double2); // Output the maximum of double1 and double2 
+    printf("\nMin:              %5.2lf", (double1 < double2) ? double1 : double2); // Output the minimum of double1 and double2
     return 0;
 }
